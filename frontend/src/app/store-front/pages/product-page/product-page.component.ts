3e8260fd5803c730ca3ce064742ccb0b27844e50
marclorenzoo <lorenzooltramarc@gmail.com>
2025-08-18 20:14:09 +0200

    Restablecer frontend como carpeta normal (sin sub-repositorio)

---
+++ frontend/src/app/store-front/pages/product-page/product-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+
+@Component({
+  selector: 'app-product-page',
+  imports: [],
+  templateUrl: './product-page.component.html',
+})
+export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  productIdSlug = this.activatedRoute.snapshot.params['idSlug'];
+  
+  productResource = rxResource({
+    params: () => ({ idSlug: this.productIdSlug }),
+    stream: ({ params }) =>
+      this.productService.getProductByIdSlug(params.idSlug),
+  });
+}
