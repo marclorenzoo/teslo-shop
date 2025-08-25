1f2f5f7b1bd8a18895399ef2df52a5bb44271e30
marclorenzoo <lorenzooltramarc@gmail.com>
2025-08-25 07:20:01 +0200

    Some updates

--- frontend/src/app/store-front/pages/product-page/product-page.component.ts
+++ frontend/src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
+import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
 export class ProductPageComponent {
@@ -13,7 +14,7 @@ export class ProductPageComponent {
   productService = inject(ProductsService);
 
   productIdSlug = this.activatedRoute.snapshot.params['idSlug'];
-  
+
   productResource = rxResource({
     params: () => ({ idSlug: this.productIdSlug }),
     stream: ({ params }) =>
